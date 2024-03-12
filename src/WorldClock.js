23e8f99b3f4984c1da6343050a26cebe688c0f16
CabsT <133862761+CabsT@users.noreply.github.com>
2024-03-12 11:27:36 +0200

    created a worldclock component to render in my application

---
+++ src/WorldClock.js
@@ -0,0 +1,15 @@
+import React from "react";
+import moment from "moment";
+
+export default function WorldClock() {
+  return (
+    <div>
+      <footer>
+        This project was coded by Charné Teichert and hosted as{" "}
+        <a href="https://github.com/CabsT/world_clock" target="_blank">
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
+}
