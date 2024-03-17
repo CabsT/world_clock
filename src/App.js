62bd537592bb6b70bd26ec39f8f4085c33acf861
CabsT <133862761+CabsT@users.noreply.github.com>
2024-03-17 14:45:02 +0200

    Added a background to the application and included a footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ function App() {
   return (
     <div className="App">
       <WorldClock />
+      <footer className="text-center">
+        This project was coded by Charné Teichert and hosted as{" "}
+        <a href="https://github.com/CabsT/world_clock" target="_blank">
+          open-sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }
